529f37ba7d0f9a1c61abf6327fd98b26fc593464
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-04-12 17:58:24 +0300

    Rework jumping

--- Assets/Scripts/PlayerMoving.cs
+++ Assets/Scripts/PlayerMoving.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEditor.UIElements;
 using UnityEngine;
 
@@ -7,23 +8,71 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMoving : MonoBehaviour
 {
+
+    [Header("Move force")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
 
+    /*    [Header("Jump Raycast Settings")]
+        [SerializeField] private Transform overlapCircleObject;
+        [SerializeField] private float jumpOffset;*/
+    [SerializeField] private string layerMask; 
 
+    [Header("Move Curve Settings")]
+    [SerializeField] private AnimationCurve curve;
+    [SerializeField] private int interpolationFramesCount;
+
+    private bool _isGrounded = false;
+    private int elapsedFrames = 0;
     private Rigidbody2D _rb;
     private Controls _controls;
-    
+    private SpriteRenderer _sp;
     private void Awake()
     {
         _controls = ControlsSingletone.GetControls();
         _rb = GetComponent<Rigidbody2D>();
+        _sp = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
     {
         Move();
+        //Debug.DrawRay(rayCastObj.position, Vector3.down * raycastDistance, Color.yellow);
     }
+
+
+    private void Move()
+    {
+
+        float direction = _controls.Move.Moving.ReadValue<float>();
+        if (direction == 1)
+        {
+            _sp.flipX = false;
+        } else if (direction == -1)
+        {
+            _sp.flipX = true;
+        }
+
+        if (direction == 0)
+            elapsedFrames = 0;
+        else
+            elapsedFrames++;
+
+
+        float interpolatedValue = Mathf.Lerp(0, direction, (float)elapsedFrames / interpolationFramesCount);
+        _rb.velocity = new Vector2(curve.Evaluate(interpolatedValue) * moveSpeed, _rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        
+        if (_isGrounded)
+        {
+            _rb.AddForce(Vector2.up * jumpForce);
+            _isGrounded = false;
+        }
+    }
+
 
     private void OnDisable()
     {
@@ -38,22 +87,27 @@ public class PlayerMoving : MonoBehaviour
 
     }
 
-
-    private void Move()
-    {
-        float direction = _controls.Move.Moving.ReadValue<float>();
-        print(direction);
-        _rb.velocity = new Vector2(direction * moveSpeed, _rb.velocity.y);
-
-    }
-
-    private void Jump()
-    {
-        _rb.AddForce(Vector2.up * jumpForce);
-    }
-
     private void OnDestroy()
     {
         ControlsSingletone.Destroy(); 
     }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.layer == LayerMask.NameToLayer(layerMask))
+        {
+            //print("_isGrounded " + _isGrounded);
+            _isGrounded = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer(layerMask))
+        {
+            //print("_isGrounded " + _isGrounded);
+            _isGrounded = false;
+        }
+    } 
+
+   
 }
