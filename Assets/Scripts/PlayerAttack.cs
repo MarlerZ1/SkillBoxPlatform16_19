60c6c5b8676bc9aad34d47d907999af41af1e0d2
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-04-14 22:15:15 +0300

    Add coldown magic attack

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -8,29 +8,44 @@ public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] private GameObject magicAttackObject;
     [SerializeField] private Transform magicAttackStartPoint;
+
+    [Header("Magic attack balance settings")]
     [SerializeField] private float magicAttackSpeed;
+    [SerializeField] private float coldownTime;
 
     private PlayerAnimationState _playerAnimationState;
     private Controls _controls;
+    private bool _canUseMagicAttack = true;
     //private SpriteRenderer _sp;
 
     private void MagicAttack()
     {
-        _playerAnimationState.HeroAttackMagic();
-        
+
+        if (_canUseMagicAttack)
+        {
+            StartCoroutine(IEMagicAttackColdown());
+            _playerAnimationState.HeroAttackMagic();
 
 
-        GameObject magicBullet = Instantiate(magicAttackObject, magicAttackStartPoint.position, Quaternion.Euler(0, transform.rotation.y == -1 ? 180 : 0, 0));
-        magicBullet.GetComponent<Bullet>().SetFrom("Player");
 
-        Rigidbody2D magicBulletRb = magicBullet.GetComponent<Rigidbody2D>();
+            GameObject magicBullet = Instantiate(magicAttackObject, magicAttackStartPoint.position, Quaternion.Euler(0, transform.rotation.y == -1 ? 180 : 0, 0));
+            magicBullet.GetComponent<Bullet>().SetFrom("Player");
 
-        //int direction = _sp.flipX ? -1 : 1;
+            Rigidbody2D magicBulletRb = magicBullet.GetComponent<Rigidbody2D>();
+
+            //int direction = _sp.flipX ? -1 : 1;
 
 
-        int direction = transform.rotation.y == -1 ? -1 : 1;
-        magicBulletRb.velocity = new Vector2(magicAttackSpeed * direction, magicBulletRb.velocity.y);
+            int direction = transform.rotation.y == -1 ? -1 : 1;
+            magicBulletRb.velocity = new Vector2(magicAttackSpeed * direction, magicBulletRb.velocity.y);
+        }
+    }
 
+    private IEnumerator IEMagicAttackColdown()
+    {
+        _canUseMagicAttack = false;
+        yield return new WaitForSeconds(coldownTime);
+        _canUseMagicAttack = true;
     }
 
 
@@ -57,6 +72,7 @@ public class PlayerAttack : MonoBehaviour
 
     private void OnDestroy()
     {
+        StopAllCoroutines();
         ControlsSingletone.Destroy();
     }
 }
