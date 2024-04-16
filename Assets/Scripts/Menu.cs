3ed9d36b6b7e65b39767e9aca9aef3fea09477c4
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-04-16 12:11:24 +0300

    Add main menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] GameObject[] onPauseActivateElements;
+    [SerializeField] GameObject[] onPauseDeactivateElements;
+
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+    }
+
+    public void ChangePauseState()
+    {
+        if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            for (int i = 0; i < onPauseActivateElements.Length; i++)
+            {
+                onPauseActivateElements[i].SetActive(true);
+            }
+        }
+        else if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            for (int i = 0; i < onPauseDeactivateElements.Length; i++)
+            {
+                onPauseDeactivateElements[i].SetActive(true);
+            }
+        }
+    }
+
+
+
+    public void LoadMainMenu()
+    {
+        LoadLvl(0);
+    }
+
+    public void ReloadCurrentLvl() 
+    {
+        LoadLvl(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadNextLvl()
+    {
+        LoadLvl(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadLvl(int lvlNumber)
+    {
+        SceneManager.LoadScene(lvlNumber);
+        Time.timeScale= 1;
+    }
+}
